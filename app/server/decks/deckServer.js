02eda75728673f326689d10ee652f02ed9dd90d9
unknown <fsutheman_15@yahoo.com>
2015-03-15 10:57:41 -0400

    Creating event and deck classes to handle SQL commands for gathering event and deck data. Restructuring server calls to use new classes

--- app/server/decks/deckServer.js
+++ app/server/decks/deckServer.js
@@ -16,7 +16,48 @@ var dataObj = function() {
 };
 dataObj.prototype = {};
 
-var event = function(params) {
+var event = function() {
+	this.data = [];
+	
+	this.dataQueryCallback = function() {};
+	this.whereClause = '1=1';
+	this.dbError = false;
+	this.errorMessage = null;
+};
+event.prototype = {};
+
+event.prototype.queryForEvents = function() {
+	var that = this, cmd =
+		'SELECT ID as eventID, Name as eventName, City as city, Country as country, StartDate as startDate, EndDate as endDate ' +
+		'FROM dba.Event ' +
+		'WHERE ActiveFlag = 1 AND ' + this.whereClause +
+		'ORDER BY EndDate DESC, Name ASC;';
+	dbase.dbResults(cmd, function(databaseData) {
+		that.queryForEventsDataCallback(databaseData);
+	});
+};
+
+event.prototype.queryForEventsDataCallback = function(databaseData) {
+	if (databaseData.dbError) {
+		this.dbError = true;
+		this.errorMessage = databaseData.errorMessage;
+	} else {
+		for (var i = 0; i < databaseData.length; i++) {
+			var newEventData = new eventData({
+				city: databaseData[i].city,
+				country: databaseData[i].country,
+				endDate: databaseData[i].endDate,
+				eventID: databaseData[i].eventID,
+				eventName: databaseData[i].eventName,
+				startDate: databaseData[i].startDate
+			});
+			this.data.push(newEventData);
+		}
+		this.dataQueryCallback();
+	}
+};
+
+var eventData = function(params) {
 	this.city = params.city ? params.city : null;
 	this.country = params.country ? params.country : null;
 	this.endDate = params.endDate ? params.endDate : null;
@@ -24,9 +65,49 @@ var event = function(params) {
 	this.eventName = params.eventName ? params.eventName : null;
 	this.startDate = params.startDate ? params.startDate : null;
 };
-event.prototype = {};
+eventData.prototype = {};
 
-var deck = function(params) {
+var deck = function() {
+	this.data = [];
+	
+	this.dataQueryCallback = function() {};
+	this.whereClause = '1=1';
+	this.dbError = false;
+	this.errorMessage = null;
+};
+deck.prototype = {};
+
+deck.prototype.queryForDecks = function() {
+	var that = this, cmd =
+		'SELECT d.ID as deckID, d.Name as name, d.CreditedTo as creditedTo, die.Place as place ' +
+		'FROM dba.DeckInEvent die ' +
+		'JOIN dba.Deck d on d.ID = die.DeckID ' +
+		'WHERE d.ActiveFlag = 1 AND ' + this.whereClause;
+	dbase.dbResults(cmd, function(databaseData) {
+		that.queryForDecksDataCallback(databaseData);
+	});
+};
+
+deck.prototype.queryForDecksDataCallback = function(databaseData) {
+	if (databaseData.dbError) {
+		this.dbError = true;
+		this.errorMessage = databaseData.errorMessage;
+	} else {
+		for (var i = 0; i < databaseData.length; i++) {
+			var newDeckData = new deckData({
+				creditedTo: databaseData[i].creditedTo,
+				deckID: databaseData[i].deckID,
+				name: databaseData[i].name,
+				place: databaseData[i].place
+			});
+			this.data.push(newDeckData);
+		}
+		
+		this.dataQueryCallback();
+	}
+};
+
+var deckData = function(params) {
 	this.creditedTo = params.creditedTo ? params.creditedTo : null;
 	this.deckID = params.deckID ? params.deckID : null;
 	this.name = params.name ? params.name : null;
@@ -34,7 +115,7 @@ var deck = function(params) {
 	
 	this.cards = params.cards ? params.cards : null;
 };
-deck.prototype = {};
+deckData.prototype = {};
 
 var card = function(params) {
 	this.cardName = params.cardName ? params.cardName : null;
@@ -46,73 +127,58 @@ var card = function(params) {
 card.prototype = {};
 
 deckServer.prototype.gatherListOfEvents = function(callback) {
-	this.queryForEvents(callback, '1=1');
-};
-
-deckServer.prototype.queryForEvents = function(callback, whereClause) {
-	var that = this, cmd = 
-		'SELECT ID as eventID, Name as eventName, City as city, Country as country, StartDate as startDate, EndDate as endDate ' +
-		'FROM dba.Event ' +
-		'WHERE ActiveFlag = 1 AND ' + whereClause +
-		'ORDER BY EndDate DESC, Name ASC;';
-	dbase.dbResults(cmd, function(databaseData) {
-		that.obj.dbError = databaseData.dbError;
-		if (databaseData.dbError) {
-			that.obj.errorMessage = databaseData.errorMessage;
-		}
-		
-		for (var i = 0; i < databaseData.length; i++) {
-			var newEvent = new event({
-				city: databaseData[i].city,
-				country: databaseData[i].country,
-				endDate: databaseData[i].endDate,
-				eventID: databaseData[i].eventID,
-				eventName: databaseData[i].eventName,
-				startDate: databaseData[i].startDate
-			});
-			that.obj.events.push(newEvent);
+	var that = this, eventBuilder = new event();
+	eventBuilder.whereClause = '1=1';
+	eventBuilder.dataQueryCallback = dataCallback;
+	
+	eventBuilder.queryForEvents();
+	
+	function dataCallback() {
+		if (eventBuilder.dbError) {
+			that.obj.dbError = true;
+			that.obj.errorMessage = eventBuilder.errorMessage;
+		} else {
+			that.obj.events = eventBuilder.data;
 		}
 		callback(that.obj);
-	});
+	}
 };
 
 deckServer.prototype.gatherSingleEventInfo = function(callback, eventID) {
-	var that = this;
+	var that = this, eventBuilder = new event(), deckBuilder = new deck();
 	
-	this.queryForEvents(gatherDecks, 'ID = ' + dbase.safeDBString(eventID));
+	eventBuilder.whereClause = 'ID = ' + dbase.safeDBString(eventID);
+	eventBuilder.dataQueryCallback = gatherEventsFinished;
+	deckBuilder.whereClause = 'die.EventID = ' + dbase.safeDBString(eventID);
+	deckBuilder.dataQueryCallback = gatherDecksFinished;
 	
-	function gatherDecks() {
-		that.queryForDecks(gatheringFinished, eventID);
+	eventBuilder.queryForEvents();
+	
+	function gatherEventsFinished() {
+		if (eventBuilder.dbError) {
+			that.obj.dbError = true;
+			that.obj.errorMessage = eventBuilder.errorMessage;
+			allGatheringFinished();
+		} else {
+			that.obj.events = eventBuilder.data;
+			deckBuilder.queryForDecks();
+		}
 	}
 	
-	function gatheringFinished() {
+	function gatherDecksFinished() {
+		if (deckBuilder.dbError) {
+			that.obj.dbError = true;
+			that.obj.errorMessage = deckBuilder.errorMessage;
+			allGatheringFinished();
+		} else {
+			that.obj.decks = deckBuilder.data;
+			allGatheringFinished();
+		}
+	}
+	
+	function allGatheringFinished() {
 		callback(that.obj);
 	}
-};
-
-deckServer.prototype.queryForDecks = function(callback, eventID) {
-	var that = this, cmd = 
-		'SELECT d.ID as deckID, d.Name as name, d.CreditedTo as creditedTo, die.Place as place ' +
-		'FROM dba.DeckInEvent die ' +
-		'JOIN dba.Deck d on d.ID = die.DeckID ' +
-		'WHERE die.EventID = ' + dbase.safeDBString(eventID) + ' and d.ActiveFlag = 1;';
-	dbase.dbResults(cmd, function(databaseData) {
-		that.obj.dbError = databaseData.dbError;
-		if (databaseData.dbError) {
-			that.obj.errorMessage = databaseData.errorMessage;
-		}
-		
-		for (var i = 0; i < databaseData.length; i++) {
-			var newDeck = new deck({
-				creditedTo: databaseData[i].creditedTo,
-				deckID: databaseData[i].deckID,
-				name: databaseData[i].name,
-				place: databaseData[i].place
-			});
-			that.obj.decks.push(newDeck);
-		}
-		callback(that.obj);
-	});
 };
 
 deckServer.prototype.gatherSingleDeckCards = function(callback, deckID) {
