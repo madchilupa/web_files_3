8293a4726effc188d4b90cfe2c73d75b5d2a3b37
unknown <fsutheman_15@yahoo.com>
2015-05-28 22:00:01 -0400

    REFACTOR: changed the layout of the dbGridBuilder server file. More consistent look with the deckServer file. dbGridBuilder only exports wrapper functions now. functinoality more properly locked into objects

--- app/server/dbGrid/dbGridBuilder.js
+++ app/server/dbGrid/dbGridBuilder.js
@@ -21,6 +21,260 @@ var gridObject = function() {
 };
 gridObject.prototype = {};
 
+gridObject.prototype.setGridName = function(newGridName) {
+	this.gridName = newGridName;
+};
+
+gridObject.prototype.isValidGrid = function() {
+	return true;
+};
+
+gridObject.prototype.findTableAndColumnInformation = function(callback) {
+	var that = this;
+	this.findBaseTableName(function() {
+		that.findColumnDefinitions(function() {
+			that.getTableInformation(function() {
+				that.assignTableAliasesToColumnInformation();
+				
+				callback();
+			});
+		});
+	});
+};
+
+gridObject.prototype.findBaseTableName = function(callback) {
+	var that = this, cmd = 'SELECT BaseTable ' +
+		'FROM dba.GridDefinition ' +
+		'WHERE GridName = \'' + dbase.safeDBString(this.gridName) + '\';';
+	
+	dbase.dbResults(cmd, function(databaseData) {
+		that.baseTableName = databaseData[0].BaseTable;
+		
+		callback();
+	});
+};
+
+gridObject.prototype.findColumnDefinitions = function(callback) {
+	var that = this, cmd = 'SELECT * ' +
+		'FROM dba.GridColumnInformation ' +
+		'WHERE GridName = \'' + dbase.safeDBString(this.gridName) + '\' ' +
+		'ORDER BY ColumnOrder ASC;';
+	
+	dbase.dbResults(cmd, function(databaseData) {
+		var columnOrderTemp = [];
+		
+		for (var i = 0; i < databaseData.length; i++) {
+			var nextColumn = new gridColumnDefinition();
+			
+			for (var columnName in databaseData[i]) {
+				if (databaseData[i].hasOwnProperty(columnName)) {
+					nextColumn.addProperty(columnName, databaseData[i][columnName]);
+				}
+			}
+			nextColumn.columnAlias = 'cAlias' + (i + 1);
+			nextColumn.calculateCellControlType();
+			
+			that.columnInfo[nextColumn.tableName + '.' + nextColumn.columnName] = nextColumn;
+			that.columnOrder.push(nextColumn.tableName + '.' + nextColumn.columnName);
+		}
+		callback();
+	});
+};
+
+gridObject.prototype.getTableInformation = function(callback) {
+	var tableAlias = 0, totalTables = 0, dbCallsFinished = 0;
+	
+	for (var tableName in this.tableInformation) {
+		if (this.tableInformation.hasOwnProperty(tableName)) {
+			totalTables++;
+		}
+	}
+	
+	for (var columnName in this.columnInfo) {
+		if (this.columnInfo.hasOwnProperty(columnName)) {
+			var columnDefinition = this.columnInfo[columnName];
+			
+			if (this.tableInformation[columnDefinition.tableName] == null) {
+				this.tableInformation[columnDefinition.tableName] = new tableInformation();
+				totalTables++;
+			}
+		}
+	}
+	
+	for (var tableName in this.tableInformation) {
+		if (this.tableInformation.hasOwnProperty(tableName)) {
+			this.tableInfoDBCall(tableName, tableAlias, databaseCallbackStack);
+			tableAlias++;
+		}
+	}
+	
+	function databaseCallbackStack() {
+		dbCallsFinished++;
+		
+		if (dbCallsFinished == totalTables) {
+			callback();
+		}
+	}
+};
+
+gridObject.prototype.tableInfoDBCall = function(tableName, tableAlias, callback) {
+	var that = this, cmd = 'SELECT * ' +
+		'FROM dba.TableInformationView ' +
+		'WHERE TableName = \'' + dbase.safeDBString(tableName) + '\';';
+			
+	dbase.dbResults(cmd, function(databaseData) {
+		var nextTable = new tableInformation();
+		
+		for (var columnName in databaseData[0]) {
+			if (databaseData[0].hasOwnProperty(columnName)) {
+				nextTable.addProperty(columnName, databaseData[0][columnName]);
+			}
+		}
+		nextTable.tableAlias = 'tAlias' + tableAlias;
+		nextTable.tableName = tableName;
+		
+		that.tableInformation[tableName] = nextTable;
+		callback();
+	});
+};
+
+gridObject.prototype.assignTableAliasesToColumnInformation = function()
+{
+	for (var columnDefinition in this.columnInfo) {
+		if (this.columnInfo.hasOwnProperty(columnDefinition)) {
+			var column = this.columnInfo[columnDefinition];
+			column.tableAlias = this.tableInformation[column.tableName].tableAlias;
+			column.columnAlias = column.tableAlias + column.columnAlias;
+		}
+	}
+};
+
+gridObject.prototype.queryForData = function(pageNumber, callback) {
+	var cmd = this.buildDataSQL(pageNumber), newRow, newCell, column, that = this;
+	dbase.dbResults(cmd, function(databaseData) {
+		for (var i = 0; i < databaseData.length; i++) {
+			newRow = {};
+			newRow.rowID = i + 1;
+			newRow.columns = [];
+			newRow.changes = false;
+			
+			for (var j = 0; j < that.columnOrder.length; j++) {
+				newCell = new cellData();
+				column = that.columnInfo[that.columnOrder[j]];
+				
+				newCell.columnName = column.columnName;
+				newCell.tableName = column.tableName;
+				newCell.valueDisplayed = databaseData[i][column.columnAlias];
+				newCell.origValue = databaseData[i][column.columnAlias];
+				newCell.columnType = column.columnType;
+				newCell.readOnly = column.readOnly;
+				newCell.changed = false;
+				newRow.columns.push(newCell);
+			}
+			that.rowData.push(newRow);
+		}
+		callback();
+	});
+};
+
+gridObject.prototype.buildDataSQL = function(pageNumber) {
+	this.buildPagingStatement(pageNumber);
+	this.buildSelectStatement();
+	this.buildFromStatement();
+	this.buildWhereStatement();
+	
+	return this.pagingSQL + ' ' + this.selectSQL + ' ' + this.fromSQL + ' ' + this.whereSQL;
+};
+
+gridObject.prototype.buildPagingStatement = function(pageNumber) {
+	//this.pagingSQL = 'SELECT TOP 500 START AT ' + ((pageNumber - 1) * 10 + 1);
+	this.pagingSQL = 'SELECT TOP 5 START AT ' + ((pageNumber - 1) * 10 + 1);
+};
+
+gridObject.prototype.buildSelectStatement = function() {
+	this.selectSQL = '';
+	
+	for (var columnKey in this.columnInfo) {
+		if (this.columnInfo.hasOwnProperty(columnKey)) {
+			var column = this.columnInfo[columnKey];
+			this.selectSQL += ' ' + dbase.safeDBString(column.tableAlias) + '.' + dbase.safeDBString(column.columnName) + ' as ' +
+				dbase.safeDBString(column.columnAlias) + ',';
+		}
+	}
+	this.selectSQL = removeLastComma(this.selectSQL);
+};
+
+gridObject.prototype.buildFromStatement = function() {
+	this.fromSQL = 'FROM dba.' + dbase.safeDBString(this.baseTableName) + ' AS ' +
+		dbase.safeDBString(this.tableInformation[this.baseTableName].tableAlias);
+	
+	for (var tableName in this.tableInformation) {
+		if (this.tableInformation.hasOwnProperty(tableName)) {
+			if (tableName != this.baseTableName) {
+				this.fromSQL += ' JOIN dba.' + dbase.safeDBString(tableName) + ' AS ' +
+					dbase.safeDBString(this.tableInformation[tableName].tableAlias);
+			}
+		}
+	}
+};
+
+gridObject.prototype.buildWhereStatement = function() {
+	this.whereSQL = ''
+}
+
+gridObject.prototype.returnFinalData = function() {
+	var returnData = {};
+	returnData.gridName = this.gridName;
+	returnData.rows = this.rowData;
+	returnData.possibleChanges = false;
+	returnData.gridProperties = this.gridProperties;
+	return returnData;
+};
+
+gridObject.prototype.buildPresentationData = function(callback) {
+	var that = this, cmd = 'SELECT * ' + 
+		'FROM dba.GridColumnInformation ' + 
+		'WHERE GridName = \'' + dbase.safeDBString(this.gridName) + '\' ' + 
+		'ORDER BY ColumnOrder ASC';
+	dbase.dbResults(cmd, function(databaseData) {
+		that.saveGridProperties(databaseData, callback);
+	});
+};
+
+gridObject.prototype.saveGridProperties = function(databaseData, callback) {
+	this.gridProperties = {};
+	for (var i = 0; i < databaseData.length; i++) {
+		this.gridProperties[databaseData[i].ColumnName] = databaseData[i];
+	}
+	callback();
+}
+
+gridObject.prototype.saveGridData = function(postData) {
+	//for each row, find which columns changed.
+		//build update statement
+		//	determine PK value for row, and primary table
+		//	determine which columns that didn't change need to be part of the update statement
+		
+	//Final SQL should look like:
+	//	UPDATE dba.TableName SET ColumnName1 = Value1, ColumnName2 = Value2 WHERE PKColumn = PKValue;
+	for (var row in postData) {
+		if (postData.hasOwnProperty(row)) {
+			var sql = '';
+			sql += ' SET';
+			for (var col in row.columns) {
+				if (row.columns.hasOwnProperty(col)) {
+					if (col.changed == true) {
+						sql += ' ' + dbase.safeDBString(col.columnName) + ' = ' + dbase.safeDBString(col.valueDisplayed) + ',';
+					}
+				}
+			}
+			sql = removeLastComma(sql);
+			
+			sql += ' WHERE ';
+		}
+	}
+};
+
 var tableInformation = function() {
 	this.tableName = null;
 	this.tableAlias = null;
@@ -118,142 +372,7 @@ var cellData = function() {
 };
 cellData.prototype = {};
 
-var currentGrid = new gridObject();
-
-function findBaseTableName(callback) {
-	var cmd = 'SELECT BaseTable ' +
-		'FROM dba.GridDefinition ' +
-		'WHERE GridName = \'' + dbase.safeDBString(currentGrid.gridName) + '\';';
-	
-	dbase.dbResults(cmd, function(databaseData) {
-		currentGrid.baseTableName = databaseData[0].BaseTable;
-		
-		callback();
-	});
-};
-
-function findColumnDefinitions(callback) {
-	var cmd = 'SELECT * ' +
-		'FROM dba.GridColumnInformation ' +
-		'WHERE GridName = \'' + dbase.safeDBString(currentGrid.gridName) + '\' ' +
-		'ORDER BY ColumnOrder ASC;';
-	
-	dbase.dbResults(cmd, function(databaseData) {
-		var columnOrderTemp = [];
-		
-		for (var i = 0; i < databaseData.length; i++) {
-			var nextColumn = new gridColumnDefinition();
-			
-			for (var columnName in databaseData[i]) {
-				if (databaseData[i].hasOwnProperty(columnName)) {
-					nextColumn.addProperty(columnName, databaseData[i][columnName]);
-				}
-			}
-			nextColumn.columnAlias = 'cAlias' + (i + 1);
-			nextColumn.calculateCellControlType();
-			
-			currentGrid.columnInfo[nextColumn.tableName + '.' + nextColumn.columnName] = nextColumn;
-			currentGrid.columnOrder.push(nextColumn.tableName + '.' + nextColumn.columnName);
-		}
-		callback();
-	});
-};
-
-function getTableInformation(callback) {
-	var tableAlias = 0, totalTables = 0, dbCallsFinished = 0;
-	
-	for (var tableName in currentGrid.tableInformation) {
-		if (currentGrid.tableInformation.hasOwnProperty(tableName)) {
-			totalTables++;
-		}
-	}
-	
-	for (var columnName in currentGrid.columnInfo) {
-		if (currentGrid.columnInfo.hasOwnProperty(columnName)) {
-			var columnDefinition = currentGrid.columnInfo[columnName];
-			
-			if (currentGrid.tableInformation[columnDefinition.tableName] == null) {
-				currentGrid.tableInformation[columnDefinition.tableName] = new tableInformation();
-				totalTables++;
-			}
-		}
-	}
-	
-	for (var tableName in currentGrid.tableInformation) {
-		if (currentGrid.tableInformation.hasOwnProperty(tableName)) {
-			tableInfoDBCall(tableName, tableAlias, databaseCallbackStack);
-			tableAlias++;
-		}
-	}
-	
-	function databaseCallbackStack() {
-		dbCallsFinished++;
-		
-		if (dbCallsFinished == totalTables) {
-			callback();
-		}
-	}
-};
-
-function tableInfoDBCall(tableName, tableAlias, callback) {
-	var cmd = 'SELECT * ' +
-		'FROM dba.TableInformationView ' +
-		'WHERE TableName = \'' + dbase.safeDBString(tableName) + '\';';
-			
-	dbase.dbResults(cmd, function(databaseData) {
-		var nextTable = new tableInformation();
-		
-		for (var columnName in databaseData[0]) {
-			if (databaseData[0].hasOwnProperty(columnName)) {
-				nextTable.addProperty(columnName, databaseData[0][columnName]);
-			}
-		}
-		nextTable.tableAlias = 'tAlias' + tableAlias;
-		nextTable.tableName = tableName;
-		
-		currentGrid.tableInformation[tableName] = nextTable;
-		callback();
-	});
-};
-
-function assignTableAliasesToColumnInformation()
-{
-	for (var columnDefinition in currentGrid.columnInfo) {
-		if (currentGrid.columnInfo.hasOwnProperty(columnDefinition)) {
-			var column = currentGrid.columnInfo[columnDefinition];
-			column.tableAlias = currentGrid.tableInformation[column.tableName].tableAlias;
-			column.columnAlias = column.tableAlias + column.columnAlias;
-		}
-	}
-};
-
-function buildDataSQL(pageNumber) {
-	buildPagingStatement(pageNumber);
-	buildSelectStatement();
-	buildFromStatement();
-	buildWhereStatement();
-	
-	return currentGrid.pagingSQL + ' ' + currentGrid.selectSQL + ' ' + currentGrid.fromSQL + ' ' + currentGrid.whereSQL;
-};
-
-function buildPagingStatement(pageNumber) {
-	//currentGrid.pagingSQL = 'SELECT TOP 500 START AT ' + ((pageNumber - 1) * 10 + 1);
-	currentGrid.pagingSQL = 'SELECT TOP 5 START AT ' + ((pageNumber - 1) * 10 + 1);
-};
-
-function buildSelectStatement() {
-	currentGrid.selectSQL = '';
-	
-	for (var columnKey in currentGrid.columnInfo) {
-		if (currentGrid.columnInfo.hasOwnProperty(columnKey)) {
-			var column = currentGrid.columnInfo[columnKey];
-			currentGrid.selectSQL += ' ' + dbase.safeDBString(column.tableAlias) + '.' + dbase.safeDBString(column.columnName) + ' as ' +
-				dbase.safeDBString(column.columnAlias) + ',';
-		}
-	}
-	currentGrid.selectSQL = removeLastComma(currentGrid.selectSQL);
-};
-
+/**********************************************************/
 function removeLastComma(input) {
 	if (input.lastIndexOf(',') == input.length - 1) {
 		return input.slice(0, input.length - 2);
@@ -262,125 +381,44 @@ function removeLastComma(input) {
 	}
 };
 
-function buildFromStatement() {
-	currentGrid.fromSQL = 'FROM dba.' + dbase.safeDBString(currentGrid.baseTableName) + ' AS ' +
-		dbase.safeDBString(currentGrid.tableInformation[currentGrid.baseTableName].tableAlias);
-	
-	for (var tableName in currentGrid.tableInformation) {
-		if (currentGrid.tableInformation.hasOwnProperty(tableName)) {
-			if (tableName != currentGrid.baseTableName) {
-				currentGrid.fromSQL += ' JOIN dba.' + dbase.safeDBString(tableName) + ' AS ' +
-					dbase.safeDBString(currentGrid.tableInformation[tableName].tableAlias);
-			}
-		}
-	}
+function resetGrid() {
+	serverObject = new gridObject();
 };
 
-function buildWhereStatement() {
-	currentGrid.whereSQL = ''
-}
-
-function saveGridProperties(databaseData, callback) {
-	currentGrid.gridProperties = {};
-	for (var i = 0; i < databaseData.length; i++) {
-		currentGrid.gridProperties[databaseData[i].ColumnName] = databaseData[i];
-	}
-	callback();
-}
-
-module.exports.resetGrid = function() {
-	currentGrid = new gridObject();
+function setGridName(newGridName) {
+	serverObject.setGridName(newGridName);
 };
 
-module.exports.setGridName = function(newGridName) {
-	currentGrid.gridName = newGridName;
+function isValidGrid() {
+	return serverObject.isValidGrid();
 };
 
-module.exports.isValidGrid = function() {
-	return true;
+function findTableAndColumnInformation(callback) {
+	return serverObject.findTableAndColumnInformation(callback);
 };
 
-module.exports.findTableAndColumnInformation = function(callback) {
-	findBaseTableName(function() {
-		findColumnDefinitions(function() {
-			getTableInformation(function() {
-				assignTableAliasesToColumnInformation();
-				
-				callback();
-			});
-		});
-	});
+function queryForData(pageNumber, callback) {
+	return serverObject.queryForData(pageNumber, callback);
 };
 
-module.exports.queryForData = function(pageNumber, callback) {
-	var cmd = buildDataSQL(pageNumber), newRow, newCell, column;
-	dbase.dbResults(cmd, function(databaseData) {
-		for (var i = 0; i < databaseData.length; i++) {
-			newRow = {};
-			newRow.rowID = i + 1;
-			newRow.columns = [];
-			newRow.changes = false;
-			
-			for (var j = 0; j < currentGrid.columnOrder.length; j++) {
-				newCell = new cellData();
-				column = currentGrid.columnInfo[currentGrid.columnOrder[j]];
-				
-				newCell.columnName = column.columnName;
-				newCell.tableName = column.tableName;
-				newCell.valueDisplayed = databaseData[i][column.columnAlias];
-				newCell.origValue = databaseData[i][column.columnAlias];
-				newCell.columnType = column.columnType;
-				newCell.readOnly = column.readOnly;
-				newCell.changed = false;
-				newRow.columns.push(newCell);
-			}
-			currentGrid.rowData.push(newRow);
-		}
-		callback();
-	});
+function returnFinalData() {
+	return serverObject.returnFinalData();
 };
 
-module.exports.returnFinalData = function() {
-	var returnData = {};
-	returnData.gridName = currentGrid.gridName;
-	returnData.rows = currentGrid.rowData;
-	returnData.possibleChanges = false;
-	returnData.gridProperties = currentGrid.gridProperties;
-	return returnData;
+function buildPresentationData(callback) {
+	return serverObject.buildPresentationData(callback);
 };
 
-module.exports.buildPresentationData = function(callback) {
-	var cmd = 'SELECT * ' + 
-		'FROM dba.GridColumnInformation ' + 
-		'WHERE GridName = \'' + dbase.safeDBString(currentGrid.gridName) + '\' ' + 
-		'ORDER BY ColumnOrder ASC';
-	dbase.dbResults(cmd, function(databaseData) {
-		saveGridProperties(databaseData, callback);
-	});
+function saveGridData(postData) {
+	return serverObject.saveGridData(postData);
 };
 
-module.exports.saveGridData = function(postData) {
-	//for each row, find which columns changed.
-		//build update statement
-		//	determine PK value for row, and primary table
-		//	determine which columns that didn't change need to be part of the update statement
-		
-	//Final SQL should look like:
-	//	UPDATE dba.TableName SET ColumnName1 = Value1, ColumnName2 = Value2 WHERE PKColumn = PKValue;
-	for (var row in postData) {
-		if (postData.hasOwnProperty(row)) {
-			var sql = '';
-			sql += ' SET';
-			for (var col in row.columns) {
-				if (row.columns.hasOwnProperty(col)) {
-					if (col.changed == true) {
-						sql += ' ' + dbase.safeDBString(col.columnName) + ' = ' + dbase.safeDBString(col.valueDisplayed) + ',';
-					}
-				}
-			}
-			sql = removeLastComma(sql);
-			
-			sql += ' WHERE ';
-		}
-	}
-};
+var serverObject = new gridObject();
+module.exports.resetGrid = resetGrid;
+module.exports.setGridName = setGridName;
+module.exports.isValidGrid = isValidGrid;
+module.exports.findTableAndColumnInformation = findTableAndColumnInformation;
+module.exports.queryForData = queryForData;
+module.exports.returnFinalData = returnFinalData;
+module.exports.buildPresentationData = buildPresentationData;
+module.exports.saveGridData = saveGridData;
